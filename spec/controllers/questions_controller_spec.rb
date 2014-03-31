744c7881b2c4a900c62850ca0a4e1fe319b74a98
Jacob Flohre <jacobflohre@Jacobs-MacBook-Pro.local>
2014-03-30 20:57:19 -0400

    Add in First Test for Questiion1

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe QuestionsController do
+describe "QuestionsController" do
 	describe "question1" do
 	
 		it "should have the content 'Welcome Big Astronaut'" do
-     	visit 'questions/question1'
+     	visit '/questions/question1'
      	expect(page).to have_content('Welcome Big Astronaut')
     end
 	end
